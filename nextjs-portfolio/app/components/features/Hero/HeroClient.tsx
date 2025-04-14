bf7582744fcce62d1fd9f393c200cd6bc457a2cc
koen de vulder <devulderk@gmail.com>
2025-04-14 16:03:52 +0200

    added arrow

--- nextjs-portfolio/app/components/features/Hero/HeroClient.tsx
+++ nextjs-portfolio/app/components/features/Hero/HeroClient.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { motion } from "motion/react";
 import { Bio } from "./components/Bio";
 import { ContactButton } from "./components/ContactButton";
 import { Heading } from "./components/Heading";
 import { Stats } from "./components/stats/Stats";
 import { useWorkingHours } from "./hooks/useWorkingHours";
+
 interface HeroClientProps {
 	projectsCount: number;
 	yearsExperience: number;
@@ -44,6 +46,55 @@ export function HeroClient({
 					/>
 				</div>
 			</div>
+
+			{/* Arrow SVG Animation */}
+			<motion.svg
+				id="banner-arrow-svg"
+				width="376"
+				height="111"
+				viewBox="0 0 376 111"
+				fill="transparent"
+				xmlns="http://www.w3.org/2000/svg"
+				className="absolute bottom-20 left-1/2 -translate-x-1/2 z-0"
+				initial={{ opacity: 0, y: 0 }}
+				animate={{
+					opacity: 1,
+					y: [0, 15, 0],
+					transition: {
+						opacity: { duration: 1.5, ease: "easeInOut" },
+						y: {
+							duration: 2,
+							ease: "easeInOut",
+							repeat: Number.POSITIVE_INFINITY,
+							repeatType: "reverse",
+							delay: 1.2, // Start moving after fading in to 75%
+						},
+					},
+				}}
+				aria-labelledby="arrow-title"
+			>
+				<title id="arrow-title">Scroll down arrow</title>
+				<motion.path
+					className="svg-arrow svg-arrow-1"
+					d="M1 1V39.9286L188 110V70.6822L1 1Z"
+					stroke="#2C2C2C"
+					initial={{ pathLength: 0 }}
+					animate={{
+						pathLength: 1,
+						transition: { duration: 1.8, ease: "easeInOut" },
+					}}
+				/>
+				<motion.path
+					className="svg-arrow svg-arrow-2"
+					d="M375 1V39.9286L188 110V70.6822L375 1Z"
+					stroke="#2C2C2C"
+					initial={{ pathLength: 0 }}
+					animate={{
+						pathLength: 1,
+						transition: { duration: 1.8, ease: "easeInOut", delay: 0.2 },
+					}}
+				/>
+			</motion.svg>
 		</div>
 	);
 }
